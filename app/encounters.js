15f19d9662a22f911fbed2a314db20b7b888ece0
Apiwat.S <hohokz@hotmail.com>
2023-01-10 19:07:17 +0700

    refeact:delete console.log

--- app/encounters.js
+++ app/encounters.js
@@ -6,12 +6,8 @@ const encountersRouter = Router();
 // Get Pantients encounter
 
 encountersRouter.get("/", async (req, res) => {
-    console.log("here 1")
 
     const results = await pool.query(`SELECT * FROM encounters WHERE is_delete=0`)
-
-    console.log("here 2")
-
     return res.json({
         data: results.rows
     });
